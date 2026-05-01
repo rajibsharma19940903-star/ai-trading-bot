1d9902e2d038b8c34e033d791b8649b3b690d9df
rajibsharma19940903-star <rajibsharma19940903@gmail.com>
2026-05-01 22:54:11 +0530

    Update bot token and enhance message sending
    Updated the bot token and modified the send function to print the response from the Telegram API. Changed the message sent in the loop.

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 import requests
 import time
 
-TOKEN = "8796123004:AAGDjAhppxlCqwqT_peny2SiTVLUFn03aqU"
+TOKEN = "8796123004:AAGDjAhppxlCqwqT_peny2SiTVLUFnO3aqU"
 CHAT_ID = "5722800652"
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {"chat_id": CHAT_ID, "text": msg}
-    requests.post(url, data=data)
+    res = requests.post(url, data=data)
+    print(res.text)
 
-# Start message
-send("✅ Bot successfully running 🚀")
-
-# Keep bot alive (VERY IMPORTANT)
+# हर 1 मिनट me message bhejega (test ke liye)
 while True:
+    send("✅ Bot running perfectly 🚀")
     time.sleep(60)
